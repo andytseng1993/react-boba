1a41ed27325502bb68db708a0d4bc396ed8aa55e
Andy Tseng <anby1611@gmail.com>
2022-03-04 22:04:28 +0000

    Nav CSS and NavLink

---
+++ src/component/layout/Navigation.js
@@ -0,0 +1,41 @@
+import { Outlet, NavLink } from "react-router-dom";
+import classes from "./Navigation.module.css";
+
+function Navigation() {
+  return (
+    <header className={classes.header}>
+      <div className={classes.logo}>Boba Milk Shop</div>
+      <nav>
+        <NavLink
+          style={({ isActive }) =>
+            isActive
+              ? {
+                  backgroundColor: "rgb(222, 166, 85)",
+                  color: "rgb(121, 30, 30)",
+                }
+              : { color: "rgb(105, 86, 74)" }
+          }
+          to="/"
+        >
+          All BobaShops
+        </NavLink>
+        <NavLink
+          style={({ isActive }) =>
+            isActive
+              ? {
+                  backgroundColor: "rgb(222, 166, 85)",
+                  color: "rgb(121, 30, 30)",
+                }
+              : { color: "rgb(105, 86, 74)" }
+          }
+          to="/newbobashop"
+        >
+          Add Boba Shop
+        </NavLink>
+      </nav>
+      <Outlet />
+    </header>
+  );
+}
+
+export default Navigation;
